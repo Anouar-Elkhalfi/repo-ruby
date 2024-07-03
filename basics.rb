b0b57701b695c61c4acac3375fe6d060c73bcb54
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-07-03 15:22:53 +0200

    methods

--- basics.rb
+++ basics.rb
@@ -3,4 +3,22 @@
   puts "anouar".upcase
   puts "rida".capitalize
   puts "anouar".reverse
-  # puts my_object.methods.sort
+  # puts my_object.methods.sort ( sort all methods for this object see ruby documentation)
+  puts my_object.length
+  puts my_object.include?("r")
+  puts my_object[0]
+  puts my_object[1..3]
+  puts my_object.gsub("a", "A")
+  # Built-in objects
+  puts"Sponge Bob".class              #=> String
+  puts 12.class                        #=> Integer
+  puts 3.14.class                      #=> Float
+  puts ["Sponge Bob", 12, 3.14].class  #=> Array
+  # puts ["Sponge Bob", 12, 3.14].methods.sort
+  array = ["Sponge Bob", 12, 3.14]
+  puts array[0]
+  puts array.first
+  puts array.last
+  # puts true.class                      #=> TrueClass
+  # puts false.class                     #=> FalseClass
+  # puts (1..100).class                  #=> Range
