0e6c29dc41cccb1ca411e674daa9367969e49cc0
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-08-08 11:58:40 +0200

    ruby

--- basics.rb
+++ basics.rb
@@ -1,6 +1,7 @@
   my_object = "anouar"
   # puts my_object.methods.sort ( sort all methods for this object see ruby documentation)
   # Built-in objects
+  puts my_object
   puts"Sponge Bob".class              #=> String
   puts 12.class                        #=> Integer
   puts 3.14.class                      #=> Float
@@ -8,5 +9,9 @@
   puts true.class                      #=> TrueClass
   puts false.class                     #=> FalseClass
   puts (1..100).class                  #=> Range
+  puts "what's your name"
+  name = gets.chomp
+  puts "how old are you ?"
+  age = gets.chomp.to_i
+  puts "hello, your name is #{name} and your age is #{age} years old"
 # exercices
-
